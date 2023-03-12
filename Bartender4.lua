1a8780d74df2a5e29aab640de728d69bd0f4e828
bntlyfp <71097726+bntlyfp@users.noreply.github.com>
2023-03-12 12:57:41 -0400

    Add files via upload

---
+++ Bartender4.lua
@@ -0,0 +1,177 @@
+
+Bartender4DB = {
+	["namespaces"] = {
+		["ActionBars"] = {
+			["profiles"] = {
+				["Adunaphel - Smolderthorn (Vanilla+)"] = {
+					["actionbars"] = {
+						{
+							["rows"] = 2,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 89,
+								["x"] = -287,
+								["point"] = "BOTTOM",
+								["scale"] = 1.05,
+							},
+							["padding"] = 0,
+						}, -- [1]
+						{
+							["enabled"] = false,
+							["version"] = 3,
+							["position"] = {
+								["y"] = -189.5000120019606,
+								["x"] = -231.4999399218102,
+								["point"] = "CENTER",
+							},
+						}, -- [2]
+						{
+							["rows"] = 2,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 89,
+								["x"] = 50,
+								["point"] = "BOTTOM",
+								["scale"] = 1.05,
+							},
+							["padding"] = 0,
+						}, -- [3]
+						{
+							["enabled"] = false,
+							["version"] = 3,
+							["position"] = {
+								["y"] = -113.5000407246012,
+								["x"] = -231.4999399218102,
+								["point"] = "CENTER",
+							},
+						}, -- [4]
+						{
+							["rows"] = 5,
+							["buttons"] = 6,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 86,
+								["x"] = -337,
+								["point"] = "BOTTOM",
+								["scale"] = 0.7,
+							},
+							["padding"] = 0,
+						}, -- [5]
+						{
+							["rows"] = 5,
+							["buttons"] = 6,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 86,
+								["x"] = 280,
+								["point"] = "BOTTOM",
+								["scale"] = 0.7,
+							},
+							["padding"] = 0,
+						}, -- [6]
+						{
+						}, -- [7]
+						[9] = {
+						},
+						[10] = {
+						},
+					},
+				},
+			},
+		},
+		["MicroMenu"] = {
+			["profiles"] = {
+				["Adunaphel - Smolderthorn (Vanilla+)"] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 0.94995,
+						["x"] = -0.95,
+						["point"] = "TOPLEFT",
+						["scale"] = 0.7,
+					},
+					["padding"] = 6,
+				},
+			},
+		},
+		["XPBar"] = {
+			["profiles"] = {
+				["Adunaphel - Smolderthorn (Vanilla+)"] = {
+					["enabled"] = true,
+					["position"] = {
+						["y"] = 9,
+						["x"] = -361.2000147205858,
+						["point"] = "BOTTOM",
+						["scale"] = 0.699999988079071,
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["BlizzardArt"] = {
+		},
+		["BagBar"] = {
+			["profiles"] = {
+				["Adunaphel - Smolderthorn (Vanilla+)"] = {
+					["enabled"] = false,
+					["version"] = 3,
+					["position"] = {
+						["y"] = 1.500011181313653,
+						["x"] = 58.50008592856636,
+						["point"] = "CENTER",
+					},
+				},
+			},
+		},
+		["StanceBar"] = {
+			["profiles"] = {
+				["Adunaphel - Smolderthorn (Vanilla+)"] = {
+					["position"] = {
+						["y"] = 123.0500068694982,
+						["x"] = 49.97516243850021,
+						["point"] = "BOTTOM",
+						["scale"] = 1,
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["Vehicle"] = {
+			["profiles"] = {
+				["Adunaphel - Smolderthorn (Vanilla+)"] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 181.0092099146999,
+						["x"] = -254.8652751550321,
+						["point"] = "BOTTOM",
+					},
+				},
+			},
+		},
+		["PetBar"] = {
+			["profiles"] = {
+				["Adunaphel - Smolderthorn (Vanilla+)"] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = 119,
+						["x"] = -310,
+						["point"] = "BOTTOM",
+						["scale"] = 0.75,
+					},
+					["padding"] = 1,
+				},
+			},
+		},
+		["RepBar"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Adunaphel - Smolderthorn (Vanilla+)"] = "Adunaphel - Smolderthorn (Vanilla+)",
+	},
+	["profiles"] = {
+		["Adunaphel - Smolderthorn (Vanilla+)"] = {
+			["minimapIcon"] = {
+				["minimapPos"] = 133.1226784619736,
+			},
+		},
+	},
+}
---
+++ MoveAnything.lua
@@ -0,0 +1,184 @@
+
+MoveAnything_CustomFrames = {
+}
+MoveAnything_CharacterSettings = {
+	["default"] = {
+		["MinimapNorthTag"] = {
+			["hidden"] = true,
+			["name"] = "MinimapNorthTag",
+		},
+		["CastingBarFrame"] = {
+			["name"] = "CastingBarFrame",
+			["pos"] = {
+				"BOTTOM", -- [1]
+				"UIParent", -- [2]
+				"BOTTOM", -- [3]
+				3.501426657938887e-005, -- [4]
+				190.0244031657547, -- [5]
+			},
+		},
+		["MinimapBorderTop"] = {
+			["hidden"] = true,
+			["name"] = "MinimapBorderTop",
+		},
+		["ConsolidatedBuffs"] = {
+			["name"] = "ConsolidatedBuffs",
+			["scale"] = 1.135804581879329,
+			["pos"] = {
+				"TOPRIGHT", -- [1]
+				"UIParent", -- [2]
+				"TOPRIGHT", -- [3]
+				-181.3258918958619, -- [4]
+				-476.0315025361224, -- [5]
+			},
+		},
+		["MiniMapLFGFrame"] = {
+			["name"] = "MiniMapLFGFrame",
+			["pos"] = {
+				"TOPLEFT", -- [1]
+				"MinimapBackdrop", -- [2]
+				"TOPLEFT", -- [3]
+				96.99974265881787, -- [4]
+				24.36364449550084, -- [5]
+			},
+		},
+		["WatchFrame"] = {
+			["originalHeight"] = 140.0000106684094,
+			["name"] = "WatchFrame",
+			["originalWidth"] = 204.0000150451927,
+			["height"] = 429.1850619564454,
+			["pos"] = {
+				"BOTTOMLEFT", -- [1]
+				"UIParent", -- [2]
+				"BOTTOMLEFT", -- [3]
+				1311.456974681198, -- [4]
+				424.1482947197324, -- [5]
+			},
+			["width"] = 205.5801388684962,
+		},
+		["MinimapZoomIn"] = {
+			["hidden"] = true,
+			["name"] = "MinimapZoomIn",
+		},
+		["MinimapZoneTextButton"] = {
+			["hidden"] = true,
+			["name"] = "MinimapZoneTextButton",
+		},
+		["MinimapCluster"] = {
+			["name"] = "MinimapCluster",
+			["scale"] = 1.086419315182934,
+			["pos"] = {
+				"TOPRIGHT", -- [1]
+				"UIParent", -- [2]
+				"TOPRIGHT", -- [3]
+				2.181347507009747, -- [4]
+				-600.000116189532, -- [5]
+			},
+		},
+		["WorldStateAlwaysUpFrame"] = {
+			["name"] = "WorldStateAlwaysUpFrame",
+			["pos"] = {
+				"TOP", -- [1]
+				"UIParent", -- [2]
+				"TOP", -- [3]
+				-685.8518605193012, -- [4]
+				-17.92597395909679, -- [5]
+			},
+		},
+		["UIErrorsFrame"] = {
+			["name"] = "UIErrorsFrame",
+			["pos"] = {
+				"TOP", -- [1]
+				"UIParent", -- [2]
+				"TOP", -- [3]
+				1.750713328969444e-005, -- [4]
+				-174.9383488352388, -- [5]
+			},
+		},
+		["TooltipMover"] = {
+			["name"] = "TooltipMover",
+			["pos"] = {
+				"TOP", -- [1]
+				"UIParent", -- [2]
+				"TOP", -- [3]
+				421.9260092469121, -- [4]
+				-402.9629477252729, -- [5]
+			},
+		},
+		["MinimapZoomOut"] = {
+			["hidden"] = true,
+			["name"] = "MinimapZoomOut",
+		},
+		["MiniMapInstanceDifficulty"] = {
+			["name"] = "MiniMapInstanceDifficulty",
+			["pos"] = {
+				"TOPLEFT", -- [1]
+				"MinimapCluster", -- [2]
+				"TOPLEFT", -- [3]
+				72.9083866075627, -- [4]
+				60.81801287061524, -- [5]
+			},
+		},
+		["PlayerFrame"] = {
+			["name"] = "PlayerFrame",
+			["scale"] = 0.4891445592586998,
+			["pos"] = {
+				"TOPLEFT", -- [1]
+				"UIParent", -- [2]
+				"TOPLEFT", -- [3]
+				1431.09484127014, -- [4]
+				-1557.265870366264, -- [5]
+			},
+		},
+		["MiniMapWorldMapButton"] = {
+			["hidden"] = true,
+			["name"] = "MiniMapWorldMapButton",
+		},
+		["ConsolidatedBuffsTooltip"] = {
+			["name"] = "ConsolidatedBuffsTooltip",
+			["pos"] = {
+				"TOPLEFT", -- [1]
+				"UIParent", -- [2]
+				"TOPLEFT", -- [3]
+				0, -- [4]
+				-4.968347980138788e-005, -- [5]
+			},
+		},
+		["DurabilityFrame"] = {
+			["name"] = "DurabilityFrame",
+			["pos"] = {
+				"BOTTOMLEFT", -- [1]
+				"UIParent", -- [2]
+				"BOTTOMLEFT", -- [3]
+				1035.086427177746, -- [4]
+				214.0184195558509, -- [5]
+			},
+		},
+		["BuffFrame"] = {
+			["name"] = "BuffFrame",
+			["scale"] = 0.8261707129298493,
+			["pos"] = {
+				"TOPRIGHT", -- [1]
+				"ConsolidatedBuffs", -- [2]
+				"TOPRIGHT", -- [3]
+				-6.171557098123904e-005, -- [4]
+				-3.284967746886204e-006, -- [5]
+			},
+		},
+		["TargetFrame"] = {
+			["name"] = "TargetFrame",
+			["scale"] = 0.856959862333571,
+			["pos"] = {
+				"BOTTOMLEFT", -- [1]
+				"UIParent", -- [2]
+				"BOTTOMLEFT", -- [3]
+				769.1273018204478, -- [4]
+				897.1797969596538, -- [5]
+			},
+		},
+	},
+}
+MoveAnything_UseCharacterSettings = nil
+MADB = {
+	["collapsed"] = true,
+}
